de61b8ad8326e05179abca31c1d9528498379edd
XIN <403042553>
2018-11-20 16:48:24 +0800

    Git dev commit

--- src/Git.java
+++ src/Git.java
@@ -2,5 +2,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("GitTest");
         System.out.println("1s");
+        System.out.println("dev");
     }
 }
