0fefd14ea4eb1835efeeed448d8c691b4fd6a34b
XIN <403042553>
2018-11-20 10:33:37 +0800

    Git commit1s

--- src/Git.java
+++ src/Git.java
@@ -1,6 +1,6 @@
 public class Git {
     public static void main(String[] args) {
         System.out.println("GitTest");
-        System.out.println("1");
+        System.out.println("1s");
     }
 }
